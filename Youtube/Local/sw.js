2a0d6179c7ed735e0a2f11613532538508a2a8bc
YSAS4331 <ryo.matsu0619@gmail.com>
2025-10-04 14:02:04 +0900

    Create sw.js

---
+++ Youtube/Local/sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'youtube-local-v1';
+const urlsToCache = [
+  '/',
+  '/index.html'
+];
+
+// インストール時にキャッシュ
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// リクエスト時にキャッシュから返す
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
