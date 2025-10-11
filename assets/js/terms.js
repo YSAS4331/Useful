587b61579a9df2e35c4fbfb20a5ff1d37dc20936
YSAS4331 <ryo.matsu0619@gmail.com>
2025-10-11 17:55:15 +0900

    Update terms.js

--- assets/js/terms.js
+++ assets/js/terms.js
@@ -1,9 +1,17 @@
+const $ = el => document.getElementById(el);
+
 class Terms extends HTMLElement {
   connectedCallback() {
     const content = this.textContent.trim();
     this.innerHTML = `
       <button id="terms">${content}</button>
     `;
+    this.addEventListener('click', () => {
+      const Frame = $('terms-frame') || document.createElement('iframe');
+      Frame.id = 'terms-frame';
+      Frame.src = 'https://ysas4331.github.io/Useful/terms';
+      if (!$('terms-frame')) document.body.appendChild(Frame);
+    })
   }
 }
 
