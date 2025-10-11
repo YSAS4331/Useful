be131ce898f65864b8ef9c9a8ae2224b6e5033a4
YSAS4331 <ryo.matsu0619@gmail.com>
2025-10-11 18:07:40 +0900

    Update terms.js

--- assets/js/terms.js
+++ assets/js/terms.js
@@ -3,19 +3,21 @@ const $ = el => document.getElementById(el);
 class Terms extends HTMLElement {
   connectedCallback() {
     const content = this.textContent.trim();
-    this.innerHTML = `
-      <button id="terms">${content}</button>
-    `;
-    this.addEventListener('click', () => {
+    this.innerHTML = `<button id="terms">${content}</button>`;
+    const btn = this.querySelector('#terms');
+
+    btn.addEventListener('click', () => {
       const Frame = $('terms-frame') || document.createElement('iframe');
       Frame.id = 'terms-frame';
       Frame.src = 'https://ysas4331.github.io/Useful/terms';
-      Frame.style.width = '250px';
-      Frame.style.height = '404.5px';
-      Frame.style.border = '2px solid black';
-      Frame.style.borderRadius = '7px';
+      Frame.style.cssText = `
+        width: 100px;
+        height: 161.8px;
+        border: 2px solid black;
+        border-radius: 7px;
+      `;
       if (!$('terms-frame')) document.body.appendChild(Frame);
-    })
+    });
   }
 }
 
