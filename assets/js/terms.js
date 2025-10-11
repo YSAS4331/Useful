05dd610d32beeb727f7365549436f3c22f0a63e8
YSAS4331 <ryo.matsu0619@gmail.com>
2025-10-11 18:05:30 +0900

    Update terms.js

--- assets/js/terms.js
+++ assets/js/terms.js
@@ -10,8 +10,8 @@ class Terms extends HTMLElement {
       const Frame = $('terms-frame') || document.createElement('iframe');
       Frame.id = 'terms-frame';
       Frame.src = 'https://ysas4331.github.io/Useful/terms';
-      Frame.style.width = '100px';
-      Frame.style.height = '161.8px';
+      Frame.style.width = '250px';
+      Frame.style.height = '404.5px';
       Frame.style.border = '2px solid black';
       Frame.style.borderRadius = '7px';
       if (!$('terms-frame')) document.body.appendChild(Frame);
