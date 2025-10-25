05b874bc28ded4893e6dfcd57484478106bcbc64
YSAS4331 <ryo.matsu0619@gmail.com>
2025-10-25 16:43:05 +0900

    Update points.js

--- assets/js/points.js
+++ assets/js/points.js
@@ -8,4 +8,6 @@ if (!pts) {
 function showToggleMenu() {
   const div = c('div');
   div.id = 'ptsToggle';
+  div.innerHTML = '当ツールでは、ポイント機能を活用することで、ユーザー体験の向上を目指しております<br>詳細につきましては、<a href="https://ysas4331.github.io/Useful/terms#points" target="_blank">こちら</a>をご覧ください。<br><br>ポイント機能をご利用になりますか？';
+  document.body.appendChild(div);
 }
