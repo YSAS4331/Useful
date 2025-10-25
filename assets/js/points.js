5905b1cb801d16abe345e0fe21bd2776fc84d077
YSAS4331 <ryo.matsu0619@gmail.com>
2025-10-25 17:54:41 +0900

    Update points.js

--- assets/js/points.js
+++ assets/js/points.js
@@ -1,14 +1,41 @@
 const c = el => document.createElement(el);
-localStorage.removeItem('pts');
+
+// localStorage.removeItem('pts'); // ← デバッグ用（毎回表示したい時に有効）
 
 const pts = localStorage.getItem('pts');
-if (!pts) {
-  showToggleMenu();
-}
+if (!pts) showToggleMenu();
 
 function showToggleMenu() {
   const div = c('div');
   div.id = 'ptsToggle';
-  div.innerHTML = '当ツールでは、ポイント機能を活用することで、ユーザー体験の向上を目指しております<br>詳細につきましては、<a href="https://ysas4331.github.io/Useful/terms#points" target="_blank">こちら</a>をご覧ください。<br><br>ポイント機能をご利用になりますか？';
+
+  div.innerHTML = `
+    <p>
+      当ツールでは、ポイント機能を活用することで、ユーザー体験の向上を目指しております。<br>
+      詳細につきましては、<a href="#">こちら</a>をご覧ください。
+    </p>
+
+    <label class="option">
+      <input type="checkbox" id="usePoints">
+      ポイント機能を利用します
+    </label>
+
+    <button id="confirmBtn">確定</button>
+  `;
+
   document.body.appendChild(div);
+
+  // --- ボタン動作 ---
+  const btn = div.querySelector('#confirmBtn');
+  const chk = div.querySelector('#usePoints');
+
+  btn.addEventListener('click', () => {
+    if (!chk.checked) {
+      alert('ポイント機能を利用する場合はチェックを入れてください。');
+      return;
+    }
+
+    localStorage.setItem('pts', 'enabled');
+    div.remove(); // モーダルを閉じる
+  });
 }
