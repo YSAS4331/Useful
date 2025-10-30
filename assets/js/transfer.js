6313163cc04683be6d303a21c8a50997bad032d8
YSAS4331 <ryo.matsu0619@gmail.com>
2025-10-30 17:53:35 +0900

    Update and rename share.js to transfer.js

---
+++ assets/js/transfer.js
@@ -0,0 +1,51 @@
+const c = el => document.createElement(el);
+
+class Transfer extends HTMLElement {
+  connectedCallback()　{
+    this.innerHTML = '<button id="transfer">データ移行</button>';
+    const btn = this.querySelector('#transfer');
+
+    btn.addEventListener('click', () => {
+      
+    });
+  }
+}
+
+customElements.define('my-transfer', Transfer);
+
+function showTransferMenu() {
+  // === オーバーレイ作成 ===
+  const overlay = c('div');
+  overlay.className = 'overlay';
+  document.body.appendChild(overlay);
+
+  // === モーダル作成 ===
+  const div = c('div');
+  div.className = 'overlayMenu';
+  div.innerHTML = `
+    <p>
+      当ツールでは、ポイント機能を活用することで、ユーザー体験の向上を目指しております。<br>
+      詳細につきましては、<a href="https://ysas4331.github.io/Useful/terms#points" target="_blank">こちら</a>をご覧ください。
+    </p>
+
+    <label class="option">
+      <input type="checkbox" id="usePoints">
+      ポイント機能を利用します
+    </label>
+
+    <button id="confirmBtn">確定</button>
+  `;
+  document.body.appendChild(div);
+
+  // === イベント設定 ===
+  const btn = div.querySelector('#confirmBtn');
+  const chk = div.querySelector('#usePoints');
+
+  btn.addEventListener('click', () => {
+    const value = chk.checked ? '0' : false;
+    localStorage.setItem('pts', value);
+    overlay.remove();
+    div.remove();
+    window.location.reload();
+  });
+}
