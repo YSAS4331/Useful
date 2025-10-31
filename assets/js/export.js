83fd0c87c9635c87e4f22be8a1aa2bfa9af86ffc
YSAS4331 <ryo.matsu0619@gmail.com>
2025-10-31 16:52:24 +0900

    Update export.js

--- assets/js/export.js
+++ assets/js/export.js
@@ -1,1 +1,24 @@
+// 便利関数
+const c = el => document.createElement(el);
+const $ = (sel, root=document) => root.querySelector(sel);
+const $$ = (sel, root = document) => Array.from(root.querySelectorAll(sel));
 
+function createPopup() {
+  // === オーバーレイ作成 ===
+  const overlay = c('div');
+  overlay.className = 'overlay';
+  document.body.appendChild(overlay);
+
+  // === モーダル作成 ===
+  const div = c('div');
+  div.className = 'overlayMenu';
+  document.body.appendChild(div);
+  function removeOverLay() {
+    overlay.remove();
+    div.remove();
+  }
+
+  return { div, removeOverLay };
+}
+
+export {c,$,$$,createPopup}
