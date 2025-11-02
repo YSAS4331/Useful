59d8154a3e08d9058d2420a56d68d194f6894396
YSAS4331 <ryo.matsu0619@gmail.com>
2025-11-02 11:09:08 +0900

    Update export.js

--- assets/js/export.js
+++ assets/js/export.js
@@ -1,3 +1,5 @@
+import CryptoJS from 'https://esm.sh/crypto-js';
+
 // 便利関数
 const c = el => document.createElement(el);
 const $ = (sel, root=document) => root.querySelector(sel);
@@ -21,4 +23,14 @@ function createPopup() {
   return { div, removeOverLay };
 }
 
-export {c,$,$$,createPopup}
+const cry = {
+  encode(data, password) {
+    return CryptoJS.AES.encrypt(data, password).toString();
+  },
+  decode(cipherText, password) {
+    const bytes = CryptoJS.AES.decrypt(cipherText, password);
+    return bytes.toString(CryptoJS.enc.Utf8);
+  }
+};
+
+export {c,$,$$,createPopup,cry}
